83a46cee5de96147b96135991688d0fb30c7e89a
Mingfu H <huangmf2000@outlook.com>
2021-05-17 08:02:11 +0000

    adding working cv tool

--- hello.py
+++ hello.py
@@ -1,14 +1,31 @@
+#!/usr/bin/env python3
+
+import click
 import boto3
 
-client = boto3.client('rekognition')
+@click.command(help="This code does label detecttion")
+@click.option('--file', prompt='I need your name of file in the bucket!',
+              help='This is the name of the file')
+def detect(file):
+    client = boto3.client('rekognition')
 
-response = client.detect_labels(
-    Image={
-        'S3Object': {
-            'Bucket': 'mingfu',
-            'Name': 'licensePlate1.jpg',
+    response = client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': 'mingfu',
+                'Name': file,
+            },
         },
-    },
-)
-
-print(response)
+    )
+    click.echo(click.style(f"Detecting labels for {file}:", fg="red"))
+    labels = response['Labels']
+    for label in labels:
+        name = label['Name']
+        confidence = label['Confidence']
+        click.echo(click.style(f"{name}: {confidence}", fg="green"))
+        #print(f"{name}: {confidence}")
+        
+        
+if __name__ == '__main__':
+    # pylint: disable=E1120
+    detect()
